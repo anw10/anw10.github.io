c7967193627b832856db1147b74325b05a191266
Arsalan Bin Najeeb <38084250+anw10@users.noreply.github.com>
2023-04-05 21:52:05 -0500

    favicon change

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ yarn run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
- echo www.arsalan.app > CNAME
+echo www.arsalan.app > CNAME
 git init
 git add -A
 git commit -m 'deploy'
